ff5ddf81666210c41eec7c1ca673dcb051cfc66a
marcelo <jmarcelovolpe@gmail.com>
2020-12-11 23:46:24 -0300

    cambio en mensaje

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bye World change to AWS!'));
+app.get('/', (req, res) => res.send('Hola gioias esta mierda esta publicada en amazon y por ahora no sirve para nada'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
